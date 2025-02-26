80f557edf1c66af479b41c524b93f07596e55dce
Phill <philldamaskin@gmail.com>
2025-02-26 10:44:37 -0500

    update namespace for generic collections

--- source/Systems/DataImportSystem.cs
+++ source/Systems/DataImportSystem.cs
@@ -1,4 +1,4 @@
-﻿using Collections;
+﻿using Collections.Generic;
 using Data.Components;
 using Data.Messages;
 using Simulation;
--- tests/ImportTests.cs
+++ tests/ImportTests.cs
@@ -1,4 +1,4 @@
-﻿using Collections;
+﻿using Collections.Generic;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
